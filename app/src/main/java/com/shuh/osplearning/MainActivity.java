1dae167b9d4420a0859b5344f2ee0fba19fc58a9
shuhjx <shuh_jx@163.com>
2017-06-05 18:16:37 +0800

    tablayout + viewpager

--- app/src/main/java/com/shuh/osplearning/MainActivity.java
+++ app/src/main/java/com/shuh/osplearning/MainActivity.java
@@ -1,14 +1,52 @@
 package com.shuh.osplearning;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+import com.shuh.osplearning.adapter.MainViewPagerAdapter;
+import com.shuh.osplearning.base.BaseActivity;
+import com.shuh.osplearning.fragment.Fragment1;
+import com.shuh.osplearning.fragment.Fragment2;
+import com.shuh.osplearning.fragment.Fragment3;
+import com.shuh.osplearning.fragment.Fragment4;
+import com.shuh.osplearning.utils.ViewUtils;
 
+import java.util.ArrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class MainActivity extends BaseActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private MainViewPagerAdapter adapter;
+    private ArrayList<Fragment> fragments;
+
+    protected void findViews(){
         setContentView(R.layout.activity_main);
+        viewPager = ViewUtils.getInstance().findViewById(this, R.id.viewPager);
+        tabLayout = ViewUtils.getInstance().findViewById(this, R.id.tabLayout);
+    }
+
+    protected void initViews(){
+        fragments = new ArrayList<>();
+        fragments.add(new Fragment1());
+        fragments.add(new Fragment2());
+        fragments.add(new Fragment3());
+        fragments.add(new Fragment4());
+        adapter = new MainViewPagerAdapter(getSupportFragmentManager(), fragments);
+        adapter.setTitles(new String[]{"首页", "发现", "进货单","我的"});
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+
+        tabLayout.addTab(tabLayout.newTab(), true);
+        tabLayout.addTab(tabLayout.newTab(), false);
+        tabLayout.addTab(tabLayout.newTab(), false);
+        tabLayout.addTab(tabLayout.newTab(), false);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabsFromPagerAdapter(adapter);
+    }
+
+    protected void setListener(){
+
     }
 }
